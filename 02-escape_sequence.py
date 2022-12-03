54b3bb11ae6a8830df78053f14b052442473ee68
Muhammad Talha <80144916+itsMuhammadtalha@users.noreply.github.com>
2022-12-03 22:03:09 +0500

    Add files via upload

---
+++ 02-escape_sequence.py
@@ -0,0 +1,12 @@
+
+print(" hello world \n i am Muhammad Talha ")
+
+# \n is the escape sequence character
+
+#we can add multiple charachers in print statement 
+
+print("hey", 6 , 7 , sep="-" , end = "009\n")
+print("Talha")
+
+# sep : the multiple parameters in print statement is sepeaeated by ~ or any other symbol [the default separator is space ]
+
---
+++ 03-datatypes.py
@@ -0,0 +1,56 @@
+# variables 
+
+a = 1
+print(a)
+
+b = "Talha"
+print(b)
+
+c = True
+print(c)
+
+d = None
+print(d)
+
+e = complex(8,4)
+print(e)
+
+#print(a+b)  #notice the output
+
+#we can print the type of operator using type function
+print("the type of a is ", type(a))
+print("the type of a is ", type(b))
+print("the type of a is ", type(c))
+print("the type of a is ", type(d))
+print("the type of a is ", type(e))
+
+#list 
+list = [8, 2.3 , [9,4], ["apple","banana"]]
+print(list)
+
+#tuple: immutable list
+tup1 = (('physics', 'chemistry'),( 1997, 2000))
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+print(tup1)
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+
+
+#dictionary : collection of key value pairs
+dict2 = {'color': 'red', 'edges': 4, 'perimeter':15}
+
+student = {
+    'name': 'Muhammad Talha',
+    'age': 21,
+    'canVote': True
+    }
+
+print(student)
+
+
+
+
+
+
+
+
