ec77c234e15adcb5dd0f91fecfdacca22519bbce
Muhammad Talha <80144916+itsMuhammadtalha@users.noreply.github.com>
2022-12-04 23:37:50 +0500

    Add files via upload

---
+++ 04-operators.py
@@ -0,0 +1,24 @@
+#operator
+
+print(5+2)
+print(5-2)
+print(5/2)
+print(5*2)
+print(5%2)
+print(5//2)
+print(5**2)  #5*5
+
+a = 5
+b = 6
+
+print("\nThe value of a+b is : ",a+b)
+print("The value of a-b is : ",a-b)
+print("The value of a/b is : ",a/b)
+print("The value of a*b is : ",a*b)
+print("The value of a%b is : ",a%b)
+print("The value of a//b is : ",a//2)
+print("The value of a raise to power b is : ",a**2)
+
+
+
+
