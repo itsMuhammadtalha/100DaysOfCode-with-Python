0b18f14be2f33b201241311784b0d155e5a33c4c
Muhammad Talha <80144916+itsMuhammadtalha@users.noreply.github.com>
2022-12-07 18:51:09 +0500

    Add files via upload

---
+++ 06-user_input.py
@@ -0,0 +1,15 @@
+a = input("Enter your name: ")
+
+print("my name is : ", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+print( x  + y ) # will be added as string
+
+
+#to add as an integer use typecasting
+
+print(int(x)+ int(y))
+
+
